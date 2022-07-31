88fcc995ab31b75dd861bfb3fbe0df64a155fecc
Frazzer951 <luke343279@gmail.com>
2022-07-31 13:48:37 -0700

    chapter 2

---
+++ chapter-2/src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+
+use rand::Rng;
+
+use colored::Colorize;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    // println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess: ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("{}", "Too small!".red()),
+            std::cmp::Ordering::Greater => println!("{}", "Too big!".red()),
+            std::cmp::Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            }
+        }
+    }
+}
